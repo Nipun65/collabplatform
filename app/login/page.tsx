6bf7ddc8cba962f12fc114a7d20e1cfb40397c77
Nipun <nipunpatel7@gmail.com>
2024-03-29 00:29:36 +0530

    changed login screen layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,18 +9,27 @@ const Login = () => {
 
   return (
     <div className="flex w-full h-screen">
-      <div className="xs:w-full lg:w-[70%] text-center h-full flex flex-col gap-7 items-center justify-center login-bg">
+      <div className="xs:w-full lg:w-[70%] h-full flex flex-col gap-7 items-center justify-center login-bg relative">
         <Image
           src={collab}
           alt="collab"
-          className="lg:absolute lg:top-3 lg:left-3 h-20 w-20"
+          className="xs:absolute xs:top-3 xs:left-3 xs:h-16 xs:w-16 lg:h-20 lg:w-20"
         />
-        <h1 className="text-[#ddd9d9] font-bold xs:text-6xl lg:text-7xl">
-          Collab With Humans
-        </h1>
-        <LoginComp status={status} className="xs:block lg:hidden" />
+        <div>
+          <h1 className="text-[#ddd9d9] xs:w-[25%] sm:w-fit md:w-fit absolute xs:left-6 xs:top-32 sm:left-6 font-bold xs:text-4xl md:text-5xl lg:text-7xl">
+            Collab<span className="xs:visible sm:hidden">.</span> With
+            <span className="xs:visible sm:hidden">.</span> Humans
+            <span className="xs:visible sm:hidden">.</span>
+          </h1>
+          <LoginComp status={status} className="xs:block lg:hidden" />
+
+          <div className="text-[#d0c1b3] font-bold tracking-wider absolute xs:bottom-32 md:bottom-24 lg:bottom-32 xs:right-6 lg:right-6 xs:text-xl md:text-3xl lg:text-4xl text-left opacity-90">
+            <p className="">Share Your Idea</p>
+            <p className="xs:ml-16 lg:ml-28">Connect With Real People</p>
+          </div>
+        </div>
       </div>
-      <div className="items-center justify-center hidden  lg:flex lg:w-[30%] ">
+      <div className="items-center justify-center bg-[#DACFC480] hidden lg:flex lg:w-[30%] ">
         <LoginComp status={status} />
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 export default function Home() {
-  return <main className="flex h-screen"></main>;
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+  return <></>;
 }
